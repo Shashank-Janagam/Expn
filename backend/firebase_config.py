a659ca9dfa5ac786f0d155f3ffc19762f322db4d
Shashank-Janagam <shashijanagam2004@gmail.com>
2025-12-02 10:49:06 +0530

    commit

--- backend/firebase_config.py
+++ backend/firebase_config.py
@@ -1,9 +1,9 @@
+import os, json
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Path to your serviceAccountKey.json file
-cred = credentials.Certificate("serviceAccountKey.json")
+service_account = json.loads(os.environ["FIREBASE_CONFIG"])
+cred = credentials.Certificate(service_account)
 
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
